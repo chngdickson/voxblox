35736039193015d98879d53c9d8eb9a12cd4789b
u22 <chngdickson@gmail.com>
2025-01-29 18:33:09 +0800

    ef

--- voxblox_ros/src/tsdf_server.cc
+++ voxblox_ros/src/tsdf_server.cc
@@ -633,6 +633,7 @@ void TsdfServer::publishMapEvent(const ros::TimerEvent& /*event*/) {
 }
 void TsdfServer::saveMeshEvent(const ros::TimerEvent&){
   if (points_C.size() == prev_cloud_size_){
+    ROS_INFO_ONCE("there has been no update for 5 seconds, generating mesh")
     generateMesh();
   }
   else{
